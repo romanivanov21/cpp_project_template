e2b7f4cb456a3cf6866cc9c31b2bcc53304d5cf5
Ivanov Roman <romanivanov21@mail.ru>
2015-08-26 23:57:44 +0300

    create project

---
+++ src/hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************
+*  	File: hello.cpp
+*	Author:
+*	Data:
+*	Descriptions:
+**************************************************/
+
+#include "test.h"
+
+#include <iostream>
+
+void print()
+{
+	std::cout<<"Hello. world!"<<std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*************************************************
+*  	File: main.cpp
+*	Author:
+*	Data:
+*	Descriptions:
+**************************************************/
+
+#include "test.h"
+
+int main(void)
+{
+	print();
+	return 0;
+}
